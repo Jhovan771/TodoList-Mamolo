e97d513a308b242978812f045c92ed62949f36fc
Jhovan Ahig <88757461+Jhovan771@users.noreply.github.com>
2022-10-04 13:43:00 +0800

    Initial commit

---
+++ Todo Mamolo/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div className='container row g-3 mt-2'>
+            <div className='col-md-9'>
+                <h1 className='float-left'>Todo List</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
